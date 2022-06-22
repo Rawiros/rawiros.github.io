8b4fddd377242b5e379a5e2840bbb4e3c81d6360
Rawir <45668076+Rawiros@users.noreply.github.com>
2022-06-22 03:05:01 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -96,8 +96,36 @@ class CardProject {
 
 // Auto init materialize
 addEventListener("DOMContentLoaded", function(){
+    // Get days to birthday
+    var DaysToBirthday = daysTo(new Date(2007, 7, 11));
+
+    // get years in fandom
+    var FandomJoined = YearDifference(new Date(2021, 1, 2), new Date())
+    var FandomYearType = Math.floor(FandomJoined) <= 1 ? "year" : "years";
+
+    // add values
+    document.querySelector("info[icon=pets]").innerText += " " + FandomJoined.toFixed(1) + " " + FandomYearType;
+    document.querySelector("info[icon=event]").innerText += getAge(new Date(2007, 7, 11)) + " — " + DaysToBirthday + " days to birthday"
+
+    Array.from(document.querySelectorAll("info[icon][tooltip][name]"))
+    .forEach(InfoElement => {
+        var p = document.createElement("p");
+        var i = document.createElement("i");
+        i.classList.add("small",  "inline-icon", "material-icons", "tooltipped");
+        i.setAttribute("data-position", "left");
+        i.setAttribute("data-tooltip", InfoElement.getAttribute("tooltip"));
+        i.innerText = InfoElement.getAttribute("icon");
+
+        p.innerHTML += "<strong>" + InfoElement.getAttribute("name") + ":  </strong>" + InfoElement.innerHTML;
+
+        p.insertBefore(i, p.querySelector("strong"));
+        InfoElement.parentElement.appendChild(p);
+        InfoElement.remove();
+        debug("Create Element", p);
+    });
+
     M.AutoInit(document.querySelector(".container"));
-    document.querySelector("div.fixed-action-btn > a").click();
+    // document.querySelector("div.fixed-action-btn > a").click();
     debug("DOMContentLoaded");
 });
 
@@ -110,6 +138,60 @@ function ShowError(err){
         classes: "red darken-4"
     })
 };
+
+/**
+ * https://stackoverflow.com/a/21090613/16329529
+ * @param {Date} date 
+ */
+function daysTo(date) {
+    var Today = new Date();
+    date.setFullYear(Today.getFullYear());
+    if(Today > date) {
+        date.setFullYear(Today.getFullYear() + 1);
+    };
+
+    // Return days
+    return Math.floor((date - Today) / (1000*60*60*24))
+};
+
+/**
+ * @param {Date} date Birthday Date
+ */
+function getAge(date){
+    var Today = new Date();
+    var Age = Today.getFullYear() - date.getFullYear()
+    var Month = Today.getMonth() - date.getMonth();
+
+    if(Month < 0 || Month == 0 && Today.getDate() < Today.getDate()) {
+        Age--;
+    };
+
+    return Age;
+};
+
+
+/**
+ * https://stackoverflow.com/a/57030608/16329529
+ * Stackoverflow because lazy asf
+ * @param {Date} olddate 
+ * @param {Date} newdate 
+ */
+function YearDifference(olddate, newdate) {
+    var new_y = newdate.getFullYear();
+    var old_y = olddate.getFullYear();
+    var diff_y = new_y - old_y;
+    var start_year = new Date(olddate);
+    var end_year = new Date(olddate);
+    start_year.setFullYear(new_y);
+    end_year.setFullYear(new_y+1);
+    if (start_year > newdate) {
+      start_year.setFullYear(new_y-1);
+      end_year.setFullYear(new_y);
+      diff_y--;
+    }
+    var diff = diff_y + (newdate - start_year)/(end_year - start_year);
+    return diff;
+}
 
 // Do request to github api
 fetch("https://api.github.com/users/Rawiros/repos")
@@ -130,5 +212,5 @@ fetch("https://api.github.com/users/Rawiros/repos")
         .filter(Project => [...Project.topics].includes("page-include"))
         .forEach(Project => DOM.Projects.appendChild(new CardProject(Project)));
 
-    }).catch(err => debug(err));
-    
+}).catch(err => debug(err));
+
